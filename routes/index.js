7cc37248f81695c4d267ab775113d7c86b315a22
naman0105 <naman0105@gmail.com>
2018-04-25 01:15:50 +0530

    improved a lot

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var openurl = require('openurl');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', open: 'camara' });
 });
-
 
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Express', open: "dummy" });
+  res.render('test', { title: 'Express', open: 'camara' });
 });
 
-router.get('/:motion', function(req,res,next){
-  var str = req.query.motion;
-  res.render('index', { title: 'Express', open: 'camara'});
+router.get('/authorize',function(req, res, next){
+  openurl.open("http://localhost:3000/test");
 })
 
+router.get('/gettickets', function(req, res, next){
+  res.render('getTickets');
+})
 
 module.exports = router;
